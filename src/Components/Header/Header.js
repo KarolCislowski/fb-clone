8e4fa2f13f92878a24a6b3c1d5748efcddfd8d1c
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-11 23:06:56 +0100

    header part 1

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,72 @@
+import './Header.css'
+
+import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import FlagIcon from '@material-ui/icons/Flag'
+import AddIcon from '@material-ui/icons/Add'
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined'
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined'
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
+import { Avatar, IconButton } from '@material-ui/core'
+
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg'
+          alt='facebook_logo'
+        />
+
+        <div className='header__input'>
+          <SearchIcon />
+          <input type='text' />
+        </div>
+      </div>
+
+      <div className='header__center'>
+        <div className='header__option header__option--active'>
+          <HomeIcon fontSize='large' />
+        </div>
+        <div className='header__option'>
+          <FlagIcon fontSize='large' />
+        </div>
+        <div className='header__option'>
+          <SubscriptionsOutlinedIcon fontSize='large' />
+        </div>
+        <div className='header__option'>
+          <StorefrontOutlinedIcon fontSize='large' />
+        </div>
+        <div className='header__option'>
+          <SupervisedUserCircleIcon fontSize='large' />
+        </div>
+
+      </div>
+
+      <div className='header__right'>
+        <div className='header__info'>
+          <Avatar />
+          <h4>DwarfDev666</h4>
+        </div>
+
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+
+      </div>
+    </div>
+  )
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0zgD_g3aGyHU-7Nc3oL0tol_F7hIxkVw",
+  authDomain: "cislowski-fb-clone.firebaseapp.com",
+  projectId: "cislowski-fb-clone",
+  storageBucket: "cislowski-fb-clone.appspot.com",
+  messagingSenderId: "302902064923",
+  appId: "1:302902064923:web:b7bb7d50fb1ca48ec745be"
+}
