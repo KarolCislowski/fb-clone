6ea24af5c876afb45ece9799bc598424984e677f
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-12 22:24:16 +0100

    end for today 12.12.2020

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -6,7 +6,6 @@ import { MessageSender } from './MessageSender/MessageSender'
 export const Feed = () => {
   return (
     <div className='feed'>
-      <h2>Feed</h2>
       <StoryReel />
       <MessageSender />
     </div>
--- src/Components/Feed/MessageSender/MessageSender.js
+++ src/Components/Feed/MessageSender/MessageSender.js
@@ -1,9 +1,49 @@
 import './MessageSender.css'
+import { Avatar } from '@material-ui/core'
+
+import VideocamIcon from '@material-ui/icons/Videocam'
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary'
+import InsertEmotionIcon from '@material-ui/icons/InsertEmoticon'
 
 export const MessageSender = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className='messageSender'>
-      <h4>MessageSender</h4>
+      <div className='messageSender__top'>
+        <Avatar />
+        <form>
+          <input
+            className='messageSender__input'
+            placeholder={`What's on your mind`}
+          />
+          <input placeholder='image URL (Optional)' />
+
+          <button
+            onClick={handleSubmit}
+            type='submit'
+          >
+            Hidden submit
+          </button>
+        </form>
+      </div>
+
+      <div className='messageSender__bottom'>
+        <div className='messageSender__option'>
+          <VideocamIcon style={{ color: 'red' }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className='messageSender__option'>
+          <PhotoLibraryIcon style={{ color: 'green' }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className='messageSender__option'>
+          <InsertEmotionIcon style={{ color: 'orange' }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
     </div>
   )
 }
