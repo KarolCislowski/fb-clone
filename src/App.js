17f9af312f3852bd305d497f9fc2a0ec7aae2e13
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-14 16:55:58 +0100

    style fixes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import './App.css'
 import { Header } from './Components/Header/Header'
 import { Sidebar } from './Components/Sidebar/Sidebar'
 import { Feed } from './Components/Feed/Feed'
+import { RightBar } from './Components/RightBar/RightBar'
 
 function App() {
+  const user = 'Krasnal'
+
   return (
     <div className="app">
-      <Header />
+      {!user ? (
+        <h1>LOGIN</h1>
+      ) : (
+          <>
+            <Header />
 
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-      </div>
+            <div className='app__body'>
+              <Sidebar />
+              <Feed />
+              <RightBar />
+            </div>
+          </>
+        )}
+
     </div>
   )
 }
---
+++ src/Components/RightBar/RightBar.js
@@ -0,0 +1,14 @@
+import './RightBar.css'
+
+export const RightBar = () => {
+  return (
+    <div className='rightBar'>
+      <h4>TO DO:</h4>
+      <ul>
+        <li>Adds</li>
+        <li>B-Days</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+  )
+}
