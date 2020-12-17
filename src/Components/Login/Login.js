d2c3bfe864db756373b8a92629f853778a067ef0
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-17 17:58:16 +0100

    Login UI

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,13 @@
 import './Login.css'
+import { Button } from '@material-ui/core'
 
 export const Login = () => {
+  const signIn = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className='Login'>
+    <div className='login'>
       <div className='login__logo'>
         <img
           src='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg'
@@ -13,6 +18,12 @@ export const Login = () => {
           alt='fb_text_logo'
         />
       </div>
+      <Button
+        type='submit'
+        onClick={signIn}
+      >
+        Sign In
+      </Button>
     </div>
   )
 }
