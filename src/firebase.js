6b9eb5861e65bd10a4c9bcfc499b4461e4428d2e
Karol Cislowski <cislowski.karol@hotmail.com>
2020-12-18 19:46:28 +0100

    firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyD0zgD_g3aGyHU-7Nc3oL0tol_F7hIxkVw",
   authDomain: "cislowski-fb-clone.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: "302902064923",
   appId: "1:302902064923:web:b7bb7d50fb1ca48ec745be"
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+export default db
